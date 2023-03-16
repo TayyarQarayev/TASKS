18b59a2a1e5a87304e02fc59736ec900401dc0a9
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-03-16 22:19:28 +0400

    Proje dosyası ekle.

---
+++ Tapsiriq.1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("My name and surname Tayyar Qarayev");
+Console.WriteLine("My specialty is Compuer engineering");
+Console.WriteLine("My university is ASOIU");
+
+string myPlaceOfBirth = "My palce of birth Masalli,Kohne Alvadi";
+Console.WriteLine(myPlaceOfBirth);
+
+int myAge = 18;
+Console.WriteLine(myAge);
