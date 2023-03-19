12ba777fc8caffa60e8a4b307b9760af76489882
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-03-19 22:11:22 +0400

    TASK updeter

--- TASK-2 19.03.2023/Program.cs
+++ TASK-2 19.03.2023/Program.cs
@@ -1,2 +1,22 @@
-﻿// TASK-1 
+﻿// TASK-1
+using static System.Console;
+using static System.Convert;
+//Write("Öyrəndiyiniz proqramlaşdırma dilinin adı :");
+//string programlasdirmaDili = ReadLine();
+//Write("Ən yaxın dostunuzun adı :");
+//string yaxinDost = ReadLine();
+//Write("Qrupunuzun nömrəsi :");
+//string qurupNomresi = ReadLine();
+
+//WriteLine($"Mənim öyrəndiyim programlaşdırma dili {programlasdirmaDili}-dir,Mənim ən yaxın dostum {yaxinDost}-dır,Mənim Qrup nömrəm {yaxinDost}-dir.");
+
 // TASK-2 
+
+Write("1.Ededi daxil edin:");
+int numberOne = ToInt32(ReadLine());
+Write("2.Ededi daxil edin:");
+int numberTwo = ToInt32(ReadLine());
+
+WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
+WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
