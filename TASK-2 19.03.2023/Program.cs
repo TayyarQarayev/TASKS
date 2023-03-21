5d159b0427535350cae05daccbc9c5c325dcec25
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-03-21 16:08:09 +0400

    TASK update

--- TASK-2 19.03.2023/Program.cs
+++ TASK-2 19.03.2023/Program.cs
@@ -3,25 +3,25 @@ using static System.Console;
 using static System.Convert;
 using static System.Math;
 
-//Write("Öyrəndiyiniz proqramlaşdırma dilinin adı :");
-//string programlasdirmaDili = ReadLine();
-//Write("Ən yaxın dostunuzun adı :");
-//string yaxinDost = ReadLine();
-//Write("Qrupunuzun nömrəsi :");
-//string qurupNomresi = ReadLine();
+Write("Öyrəndiyiniz proqramlaşdırma dilinin adı :");
+string programlasdirmaDili = ReadLine();
+Write("Ən yaxın dostunuzun adı :");
+string yaxinDost = ReadLine();
+Write("Qrupunuzun nömrəsi :");
+string qurupNomresi = ReadLine();
 
-//WriteLine($"Mənim öyrəndiyim programlaşdırma dili {programlasdirmaDili}-dir,Mənim ən yaxın dostum {yaxinDost}-dır,Mənim Qrup nömrəm {yaxinDost}-dir.");
+WriteLine($"Mənim öyrəndiyim programlaşdırma dili {programlasdirmaDili}-dir,Mənim ən yaxın dostum {yaxinDost}-dır,Mənim Qrup nömrəm {yaxinDost}-dir.");
 
-//// TASK-2  
+// TASK-2  
 
-//Write("1.Ededi daxil edin:");
-//int numberOne = ToInt32(ReadLine());
-//Write("2.Ededi daxil edin:");
-//int numberTwo = ToInt32(ReadLine());
+Write("1.Ededi daxil edin:");
+int numberOne = ToInt32(ReadLine());
+Write("2.Ededi daxil edin:");
+int numberTwo = ToInt32(ReadLine());
 
-//WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
-//WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
-//WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
+WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
+WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
 
 // TASK-3 riyazi ifadenin helli
 
