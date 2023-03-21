05eb304b2d08033e3b0a8cbf28a45db7e4c11a9a
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-03-21 16:05:56 +0400

    TASK update

--- TASK-2 19.03.2023/Program.cs
+++ TASK-2 19.03.2023/Program.cs
@@ -1,22 +1,37 @@
 ﻿// TASK-1
 using static System.Console;
 using static System.Convert;
-Write("Öyrəndiyiniz proqramlaşdırma dilinin adı :");
-string programlasdirmaDili = ReadLine();
-Write("Ən yaxın dostunuzun adı :");
-string yaxinDost = ReadLine();
-Write("Qrupunuzun nömrəsi :");
-string qurupNomresi = ReadLine();
+using static System.Math;
 
-WriteLine($"Mənim öyrəndiyim programlaşdırma dili {programlasdirmaDili}-dir,Mənim ən yaxın dostum {yaxinDost}-dır,Mənim Qrup nömrəm {yaxinDost}-dir.");
+//Write("Öyrəndiyiniz proqramlaşdırma dilinin adı :");
+//string programlasdirmaDili = ReadLine();
+//Write("Ən yaxın dostunuzun adı :");
+//string yaxinDost = ReadLine();
+//Write("Qrupunuzun nömrəsi :");
+//string qurupNomresi = ReadLine();
 
-// TASK-2 
+//WriteLine($"Mənim öyrəndiyim programlaşdırma dili {programlasdirmaDili}-dir,Mənim ən yaxın dostum {yaxinDost}-dır,Mənim Qrup nömrəm {yaxinDost}-dir.");
 
-Write("1.Ededi daxil edin:");
-int numberOne = ToInt32(ReadLine());
-Write("2.Ededi daxil edin:");
-int numberTwo = ToInt32(ReadLine());
+//// TASK-2  
 
-WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
-WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
-WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
+//Write("1.Ededi daxil edin:");
+//int numberOne = ToInt32(ReadLine());
+//Write("2.Ededi daxil edin:");
+//int numberTwo = ToInt32(ReadLine());
+
+//WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+//WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
+//WriteLine($"{numberOne} * {numberTwo} = {numberOne * numberTwo}");
+
+// TASK-3 riyazi ifadenin helli
+
+double x, y, z;
+
+Write("x-i daxil edin:");
+ x = ToDouble(ReadLine());
+Write("z-i daxil edin:");
+ z = ToDouble(ReadLine());
+
+y = (Abs((Sqrt(x) * Pow(z,4.0/3)) / Pow(z,3) * Sqrt(x) - Abs(Sqrt(x)))); 
+
+WriteLine($"y = {y}");
