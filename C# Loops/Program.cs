519d8812a052e2a8bf47a35047df399d59ecc338
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-03-30 20:27:09 +0400

    30.03.2023

---
+++ C# Loops/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+using static System.Convert;
+
+//     TASK-Part 1 : 1 - dən 50 qədər 3artım ilə ədədlərin hasilini tapın 
+{
+    WriteLine("TASK-Part 1");
+    long product = 1;
+
+    for (long i = 1; i <= 50; i += 3)
+    {
+        product *= i;
+    }
+    WriteLine(long.MaxValue);
+    WriteLine($"product : {product}");
+
+}
+//   TASK - part 2 :  -30 - dan 28 - ə qədər həm 3 həm 6 bölünən ədədlərin cəmini , 5 və ya 2 -  ə bölünənlərin hasilini  və tək ədədlərin sayını tapın
+{
+    WriteLine("TASK-Part 2");
+    int sum = 0;
+    long product = 1;
+    int count = 0;
+
+    for (int i = -30; i <= 28; i++)
+    {
+        if (i % 3 == 0 && i % 6 == 0)
+        {
+            sum += i;
+        }
+    }
+    WriteLine($"sum : {sum}");
+
+    for (long i = -30; i <= 28; i++)
+    {
+        if ((i % 2 == 0 || i % 5 == 0) && i != 0) 
+        {
+            product = product * i;
+        }
+    }
+    WriteLine($"product : {product}");
+
+    for (int i = -30; i <= 28; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count++;
+        }
+    }
+    WriteLine($"count : {count}");
+}
+
+//    TASK-part 3 : 304001 ədədinin müsbət rəqəmlərinin cəmini müsbət və mənfi olmayan rəqəmlərinin sayını tapın
+
+{
+    WriteLine("TASK-Part 3");
+    {
+        int sum = 0;
+        int index = 304001;
+        int remainder = 0;
+        while (index > 0)
+        {
+            remainder = index % 10;
+            sum += remainder;
+            index = index / 10;
+        }
+        WriteLine($"sum : {sum}");
+    }
+    {
+        int index = 304001;
+        int count = 0;
+        while (index > 1)
+        {
+            index = index / 10;
+            count++;
+        }
+        WriteLine($"count : {count}");
+    }
+}
