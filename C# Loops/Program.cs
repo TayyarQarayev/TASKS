581b55c1f9bd2e299d38a8487f9da4bd68bbcbc8
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-04-03 18:55:12 +0400

    Login and Password Task add

--- C# Loops/Program.cs
+++ C# Loops/Program.cs
@@ -2,68 +2,120 @@
 using static System.Console;
 using static System.Convert;
 
-//     TASK-Part 1 : 1 - dən 50 qədər 3artım ilə ədədlərin hasilini tapın 
-{
-    WriteLine("TASK-Part 1");
-    long product = 1;
+////     TASK-Part 1 : 1 - dən 50 qədər 3artım ilə ədədlərin hasilini tapın 
+//{
+//    WriteLine("TASK-Part 1");
+//    long product = 1;
 
-    for (long i = 1; i <= 50; i += 3)
-    {
-        product *= i;
-    }
-    WriteLine(long.MaxValue);
-    WriteLine($"product : {product}");
+//    for (long i = 1; i <= 50; i += 3)
+//    {
+//        product *= i;
+//    }
+//    WriteLine(long.MaxValue);
+//    WriteLine($"product : {product}");
 
-}
-//   TASK - part 2 :  -30 - dan 28 - ə qədər həm 3 həm 6 bölünən ədədlərin cəmini , 5 və ya 2 -  ə bölünənlərin hasilini  və tək ədədlərin sayını tapın
+//}
+////   TASK - part 2 :  -30 - dan 28 - ə qədər həm 3 həm 6 bölünən ədədlərin cəmini , 5 və ya 2 -  ə bölünənlərin hasilini  və tək ədədlərin sayını tapın
+//{
+//    WriteLine("TASK-Part 2");
+//    int sum = 0;
+//    long product = 1;
+//    int count = 0;
+
+//    for (int i = -30; i <= 28; i++)
+//    {
+//        if (i % 3 == 0 && i % 6 == 0)
+//        {
+//            sum += i;
+//        }
+//        if ((i % 2 == 0 || i % 5 == 0) && i != 0)
+//        {
+//            product = product * i;
+//        }
+//        if (i % 2 != 0)
+//        {
+//            count++;
+//        }
+//    }
+//    WriteLine($"sum : {sum}");
+//    WriteLine($"product : {product}");
+//    WriteLine($"count : {count}");
+
+//}
+
+
+////    TASK - part 3 : 304001 ədədinin müsbət rəqəmlərinin cəmini müsbət və mənfi olmayan rəqəmlərinin sayını tapın
+
+//{
+//    WriteLine("TASK-Part 3");
+//    {
+//        int sum = 0;
+//        int count = 0;
+//        int index = 304001;
+//        int remainder = 0;
+//        while (index > 1)
+//        {
+//            if (index > 0)
+//            {
+//                remainder = index % 10;
+//                sum += remainder;
+//                index = index / 10;
+//                count++;
+//            }
+//        }
+//        WriteLine($"sum : {sum}");
+//        WriteLine($"count : {count}");
+//    }
+//}
+
+// TASK - Part 4
+
 {
-    WriteLine("TASK-Part 2");
-    int sum = 0;
-    long product = 1;
+
+    string login = "booleanteach";
+    string password = "booleanms13";
+    int limit = 3;
     int count = 0;
+    bool result = false;
+    string enterLogin = string.Empty;
+    string enterPassword = string.Empty;
 
-    for (int i = -30; i <= 28; i++)
+    do
     {
-        if (i % 3 == 0 && i % 6 == 0)
+        Write("Enter you Login : ");
+        enterLogin = ReadLine();
+        Write("Enter you Password : ");
+        enterPassword = ReadLine();
+        count++;
+        if(enterLogin != login && enterPassword != password)
         {
-            sum += i;
+            WriteLine("Login and Password is Wrong!");
         }
-        if ((i % 2 == 0 || i % 5 == 0) && i != 0)
+        else if (enterLogin != login)
         {
-            product = product * i;
+            WriteLine("Lgin is wrong!");
         }
-        if (i % 2 != 0)
+        else if (enterPassword != password)
         {
-            count++;
+            WriteLine("Password is wrong!");
         }
-    }
-    WriteLine($"sum : {sum}");
-    WriteLine($"product : {product}");
-    WriteLine($"count : {count}");
+        else
+        {
+            
+        }
+        if (limit == count)
+        {
+            result = true;
+        }
+    } while ((enterLogin != login && enterPassword != password)&& !result);
 
-}
-
-
-//    TASK-part 3 : 304001 ədədinin müsbət rəqəmlərinin cəmini müsbət və mənfi olmayan rəqəmlərinin sayını tapın
-
-{
-    WriteLine("TASK-Part 3");
+    if (result is false)
     {
-        int sum = 0;
-        int count = 0;
-        int index = 304001;
-        int remainder = 0;
-        while (index > 1)
-        {
-            if (index > 0)
-            {
-                remainder = index % 10;
-                sum += remainder;
-                index = index / 10;
-                count++;
-            }
-        }
-        WriteLine($"sum : {sum}");
-        WriteLine($"count : {count}");
+        WriteLine("You are login :)");
     }
+    else
+    {
+        WriteLine("System is blocked !!");
+    }
+
 }
