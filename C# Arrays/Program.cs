4d524af98b0b050eff87177b01180fae44978137
Tayyar Qarayev <Tayyar Qarayev@DESKTOP-2IGPUGJ>
2023-04-06 21:59:39 +0400

    Student list

---
+++ C# Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿//  5 elementdən ibarət tələbə massivi yaradın. Massivin dəyərlərini istifadəçidən alın. Sonra isə dəyərləri ekranda göstərin 
+
+string[] students = new string[5];
+
+for (int i = 0; i < students.Length; i++)
+{
+    Console.Write($"Student {i + 1}: ");
+    students[i] = Console.ReadLine();
+}
+
+foreach (var student in students) 
+{ 
+    Console.Write($"Student :");
+    Console.WriteLine(student);
+}
